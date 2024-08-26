c77bc8af07cf23c652a8798267c6ec6ecd796355
Dwight Guth <dwight.guth@pi2.network>
2024-08-26 11:45:02 -0500

    update test to have better coverage

--- test/regression/if.sol
+++ test/regression/if.sol
@@ -4,15 +4,21 @@ contract IfTest {
 
     constructor() {
       bool x = true;
+      bool y = false;
       if (x) {
+        y = true;
       } else {
 	require(false, "");
       }
+      require(y, "");
+      y = false;
       x = false;
       if (x) {
         require(false, "");
       } else {
+        y = true;
       }
+      require(y, "");
       if (x) {
         require(false, "");
       }
