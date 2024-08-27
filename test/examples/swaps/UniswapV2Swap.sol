a2bc149c68dfedcf564334ffc1026f038747395f
Dwight Guth <dwight.guth@pi2.network>
2024-08-27 10:05:19 -0500

    update swap contract

--- test/examples/swaps/UniswapV2Swap.sol
+++ test/examples/swaps/UniswapV2Swap.sol
@@ -54,8 +54,7 @@ contract UniswapV2Swap {
         dai.transferFrom(msg.sender, address(this), amountIn);
         dai.approve(address(router), amountIn);
 
-        address[] memory path;
-        path = new address[](3);
+        address[] memory path = new address[](3);
         path[0] = address(dai);
         path[1] = address(weth);
         path[2] = address(usdc);
@@ -72,8 +71,7 @@ contract UniswapV2Swap {
         weth.transferFrom(msg.sender, address(this), amountInMax);
         weth.approve(address(router), amountInMax);
 
-        address[] memory path;
-        path = new address[](2);
+        address[] memory path = new address[](2);
         path[0] = address(weth);
         path[1] = address(dai);
 
@@ -93,8 +91,7 @@ contract UniswapV2Swap {
         dai.transferFrom(msg.sender, address(this), amountInMax);
         dai.approve(address(router), amountInMax);
 
-        address[] memory path;
-        path = new address[](3);
+        address[] memory path = new address[](3);
         path[0] = address(dai);
         path[1] = address(weth);
         path[2] = address(usdc);
